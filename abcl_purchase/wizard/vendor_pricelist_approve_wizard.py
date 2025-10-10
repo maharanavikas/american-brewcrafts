cfb43921030dc867fb5f11e95c9f9173175b216b
Vijaya Gopala KrishnaRaju Datla - LFI <vdatla@linkfields.com>
2025-10-10 16:33:36 +0530

    Approval wizard for vendor pricelist

---
+++ abcl_purchase/wizard/vendor_pricelist_approve_wizard.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+
+class VendorPricelistApprovalWizard(models.TransientModel):
+    _name = 'vendor.pricelist.approval.wizard'
+    _description = 'Vendor Pricelist Approval Wizard'
+
+    comment = fields.Text(string="Comment", required=True)
+    product_supplierinfo_id = fields.Many2one('product.supplierinfo', string="Supplier Info", required=True)
+    action_type = fields.Selection([
+        ('approve', 'Approve'),
+        ('reject', 'Reject')
+    ], default='approve', string="Action Type")
+
+    @api.constrains('comment')
+    def _check_comment_not_empty_or_spaces(self):
+        for record in self:
+            if not record.comment or not record.comment.strip():
+                raise ValidationError("The comment cannot be empty or contain only spaces.")
+
+    def action_submit(self):
+        self.ensure_one()
+        if self.action_type == 'approve':
+            self.product_supplierinfo_id.action_approve_request()
+            self.product_supplierinfo_id.approver_comment = self.comment
+        else :
+            self.product_supplierinfo_id.action_reject_request()
+            self.product_supplierinfo_id.approver_comment = self.comment
