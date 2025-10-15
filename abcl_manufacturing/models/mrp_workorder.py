2349c0b2e8a02183f1e221a18c4ec91967d13127
Vijaya Gopala KrishnaRaju Datla - LFI <vdatla@linkfields.com>
2025-10-15 10:51:46 +0530

    Modified validations

--- abcl_manufacturing/models/mrp_workorder.py
+++ abcl_manufacturing/models/mrp_workorder.py
@@ -6,20 +6,20 @@ from odoo.exceptions import UserError, ValidationError
 class MrpProductionWorkcenterLine(models.Model):
     _inherit = 'mrp.workorder'
 
-    # def button_start(self, raise_on_invalid_state=False, bypass=False):
-    #     res = super(MrpProductionWorkcenterLine, self).button_start(raise_on_invalid_state=raise_on_invalid_state, bypass=True)
-    #     print("Work center based start button")
-    #     if self.production_id.quality_check_todo:
-    #         raise ValidationError("Please complete the Pre production Quality Checks before starting the Work Order.")
-    #     elif self.production_id.quality_check_fail:
-    #         raise ValidationError("You cannot start the Work Order as Pre production Quality Checks have failed")
+    def button_start(self, raise_on_invalid_state=False, bypass=False):
+        res = super(MrpProductionWorkcenterLine, self).button_start(raise_on_invalid_state=raise_on_invalid_state, bypass=bypass)
+        print("Inside the start button")
+        if self.production_id.quality_check_todo:
+            raise ValidationError("Please complete the Pre production Quality Checks before starting the Work Order.")
+        elif self.production_id.quality_check_fail:
+            raise ValidationError("You cannot start the Work Order as Pre production Quality Checks have failed")
     #     elif self.quality_check_todo:
     #         print("In button_start QC todo")
     #         raise ValidationError("Please complete the Quality Checks before starting the Work Order.")
     #     elif self.quality_check_fail:
     #         print("In button_start QC FAIL")
     #         raise ValidationError("You cannot start the Work Order as Quality Checks for the components have failed")
-    #     return res
+        return res
 
     def action_mark_as_done(self):
         res = super(MrpProductionWorkcenterLine, self).action_mark_as_done()
@@ -37,11 +37,11 @@ class MrpProductionWorkcenterLine(models.Model):
     def button_finish(self):
         print("Work center based finish button")
         if self.production_id.quality_check_todo:
-            raise ValidationError("Please complete the Pre production Quality Checks before finishing the Work Order.")
+            raise ValidationError("Please complete the Pre production Quality Checks before starting the Work Order.")
         elif self.production_id.quality_check_fail:
             raise ValidationError("You cannot finish the Work Order as Pre production Quality Checks for the components have failed")
         elif self.quality_check_todo:
-            raise ValidationError("Please complete the Quality Checks before finishing the Work Order.")
+            raise ValidationError("Please complete the Quality Checks before starting the Work Order.")
         elif self.quality_check_fail:
             raise ValidationError("You cannot finish the Work Order as Quality Checks for the components have failed")
         res = super(MrpProductionWorkcenterLine, self).button_finish()
