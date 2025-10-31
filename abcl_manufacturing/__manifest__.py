597f6449f3a77a2d80a1f2b255a76c4c1ad8e07e
Vijaya Gopala KrishnaRaju Datla - LFI <vdatla@linkfields.com>
2025-10-31 11:24:11 +0530

    added validation for qc save and filter on wo

--- abcl_manufacturing/__manifest__.py
+++ abcl_manufacturing/__manifest__.py
@@ -12,7 +12,7 @@
     'category': 'Manufacturing/Manufacturing',
     'version': '18.0',
 
-    'depends': ['mrp', 'mrp_mps', 'stock'],
+    'depends': ['mrp', 'mrp_mps', 'stock','product', 'abcl_purchase'],
     'data': [
         "security/ir.model.access.csv",
         "wizard/extra_production_wizard_views.xml",
@@ -20,7 +20,7 @@
         "views/mrp_bom_views.xml",
         'views/mrp_production_schedule.xml',
         'views/stock_quant_views.xml',
-
+        'views/work_order_views.xml',
     ],
     'assets': {
         'web.assets_backend': [
