27ef69e60ce7b0c35b9abb29feb9bcc0f8dfea01
Gouranga Kala <gkala@linkfields.com>
2025-11-18 13:21:29 +0530

    receipt validation added

--- abcl_purchase/models/stock_picking.py
+++ abcl_purchase/models/stock_picking.py
@@ -8,18 +8,15 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         if self.picking_type_id.code == 'incoming':
-            for move_line in self.move_ids:
-                if move_line.product_uom == move_line.purchase_line_id.product_uom:
-                    if move_line.purchase_line_id and move_line.quantity > (move_line.purchase_line_id.product_qty)-(move_line.purchase_line_id.qty_received):
-                        raise ValidationError(f"The Received Quantity for the product {move_line.name} cannot exceed Demand Quantity")
-                elif move_line.product_uom.name == 'g':
-                    kg_uom = self.env.ref('uom.product_uom_kgm')
-                    g_uom = self.env.ref('uom.product_uom_gram')
-                    qty_in_units = g_uom._compute_quantity(move_line.quantity, kg_uom)
-                    print(qty_in_units)
-                    if move_line.purchase_line_id and qty_in_units > (move_line.purchase_line_id.product_qty)-(move_line.purchase_line_id.qty_received):
-                        raise ValidationError(f"The Received Quantity for the product {move_line.name} cannot exceed Demand Quantity")
-                else:    
-                    raise ValidationError(f"The UoM for the product {move_line.name} in Stock Move Line must be same as UoM in Purchase Order Line")        
-        return super(StockPicking, self).button_validate()
+            for move_line in self.move_ids.filtered('purchase_line_id'):
+                purchase_line = move_line.purchase_line_id
+                receiving_quantity = move_line.quantity
 
+                if purchase_line.product_uom != move_line.product_uom:
+                    receiving_quantity = move_line.product_uom._compute_quantity(
+                        move_line.quantity, purchase_line.product_uom
+                    )
+                if receiving_quantity > (purchase_line.product_qty - purchase_line.qty_received):
+                    raise ValidationError(f"The Received Quantity for the product {move_line.name} cannot exceed Demand Quantity. The remaining demand quantity is {purchase_line.product_qty - purchase_line.qty_received} {purchase_line.product_uom.name}.")
+        
+        return super(StockPicking, self).button_validate()
